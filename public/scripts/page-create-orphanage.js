7032ce075720c92e82f5457c02fd8bc13808ad3d
Lincoln-Silva <71925693+Lincoln-Silva@users.noreply.github.com>
2020-10-17 13:11:41 -0300

    Add files via upload

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,96 @@
+//create map
+const map = L.map("mapid").setView([-23.5394245, -46.7993378], 15);
+
+//create and add tilelayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+//create icon
+const icon = L.icon({
+  iconUrl: "/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+});
+
+let marker;
+
+//create and add marker
+map.on("click", (event) => {
+  const lat = event.latlng.lat;
+  const lng = event.latlng.lng;
+
+  document.querySelector("[name=lat]").value = lat;
+  document.querySelector("[name=lng]").value = lng;
+
+  // remove icon
+  marker && map.removeLayer(marker);
+
+  //add icon layer
+  marker = L.marker([lat, lng], { icon }).addTo(map);
+});
+
+// adicionar o campo de fotos
+function addPhotoField() {
+  //pegar o container de fotos #images
+  const container = document.querySelector("#images");
+  //pegar o container para duplicar .new-image
+  const fieldsContainer = document.querySelectorAll(".new-upload");
+  //realizar a clonagem da ultima imagem adicionada
+  const newFieldContainer = fieldsContainer[
+    fieldsContainer.length - 1
+  ].cloneNode(true);
+  //verificar se o campo está vazio, se sim, não adicionar ao campo de imagens
+  const input = newFieldContainer.children[0];
+
+  if (input.value == "") {
+    return;
+  }
+
+  //limpar o campo antes de adicionar ao container de imagens
+  input.value = "";
+
+  //adicionar o clone ao container de ##images
+  container.appendChild(newFieldContainer);
+}
+
+function deleteField(event) {
+  const span = event.currentTarget;
+
+  const fieldsContainer = document.querySelectorAll(".new-upload");
+
+  if (fieldsContainer.length <= 1) {
+    //limpar o valor do campo
+    span.parentNode.children[0].value = "";
+    return;
+  }
+
+  //deletar o campo
+  span.parentNode.remove();
+}
+
+// troca do sim e nao
+function toggleSelect(event) {
+  // retirar a class .active (dos botoes)
+  document.querySelectorAll(".button-select button").forEach(function (button) {
+    button.classList.remove("active");
+  });
+
+  // colocar a class.active  nesse botao clicado
+  const button = event.currentTarget;
+  button.classList.add("active");
+
+  // atualizar o input hidden com o valor selecionado
+  const input = document.querySelector('[name="open_on_weekends"]');
+
+  input.value = button.dataset.value;
+}
+
+function validade(event) {
+
+  // validar se lat e lng estão preeenchidos
+  const needsLatAndLng = false;
+  if(needsLatAndLng) {
+    event.preventDefault()
+  alert('Selecione um ponto no mapa')
+  }
+  
+}
---
+++ src/database/test.js
@@ -0,0 +1,36 @@
+const Database = require('./db');
+const saveOrphanage = require('./saveOrphanage');
+
+Database.then(async db => {
+    // inserir dados na tabela
+    await saveOrphanage(db, 
+       {
+           lat:"-23.5394245",
+           lng:"-46.7993378", 
+           name: "Lar das meninas",
+           about:"Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+           whatsapp:"28912941898",
+           images: [
+   
+               "https://images.unsplash.com/photo-1599003037886-f8b50bc290c8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+               "https://images.unsplash.com/photo-1601180964546-f5c6ac9fdc14?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+          ].toString(),
+         instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+         opening_hours: "Horário de visitas Das 8h até 18h",
+         open_on_weekends: "1"
+        })
+        
+    //consultar dados da tabela
+  const selectedOrphanages = await db.all("SELECT * FROM orphanages")
+  console.log(selectedOrphanages)
+
+   // consultar somente 1 orphanato, pelo id
+   const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "2"')
+   console.log(orphanage)
+
+// //  // deletar dado da tabela
+//   console.log(await db.run('DELETE FROM orphanages WHERE id = "5"'))
+//   console.log(await db.run('DELETE FROM orphanages WHERE id = "6"'))
+
+})
